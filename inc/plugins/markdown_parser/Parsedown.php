9b522ab7f494f7705df44761d5fce4fd1949f386
volkbarks <volkbarks@gmail.com>
2024-12-26 02:00:35 -0800

    Added Parsedown.php
    Skeleton file for where the Parsedown.php class should be included.

---
+++ inc/plugins/markdown_parser/Parsedown.php
@@ -0,0 +1,2 @@
+# Replace this file with the Parsedown library. https://github.com/erusev/parsedown
+# This plugin is currently using Parsedown version Release 1.7.4: https://github.com/erusev/parsedown/releases/tag/1.7.4
